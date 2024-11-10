7ddb14c7111ca66e30b1ab2e64dcf79f56d0942a
LilBoban <dudarow.d@yandex.ru>
2024-11-10 15:36:29 +0500

    added extensions and beta console UI

---
+++ Lab3 dynamic structures/LinkedListExtensions.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab3_dynamic_structures
+{
+    public static class LinkedListExtensions
+    {
+        // 1. Функция, которая переворачивает список L
+        public static void Reverse<T>(this LinkedList<T> list)
+        {
+            if (list.Count <= 1) return;
+
+            Node<T> current = list.head;
+            Node<T> prev = null;
+            Node<T> next = null;
+
+            while (current != null)
+            {
+                next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+
+            list.tail = list.head;
+            list.head = prev;
+        }
+
+
+        // 3. Функция, которая определяет количество различных элементов списка
+        public static int CountDistinct(this LinkedList<int> list)
+        {
+            HashSet<int> distinctElements = new HashSet<int>();
+            Node<int> current = list.head;
+            while (current != null)
+            {
+                distinctElements.Add(current.Data);
+                current = current.Next;
+            }
+            return distinctElements.Count;
+        }
+
+        // 5. Функция вставки списка самого в себя после первого вхождения числа х
+        public static void InsertIntoItself<T>(this LinkedList<T> list, T x)
+        {
+            Node<T> nodeX = null;
+            Node<T> current = list.head;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(x))
+                {
+                    nodeX = current;
+                    break;
+                }
+                current = current.Next;
+            }
+
+            if (nodeX != null)
+            {
+                LinkedList<T> copy = new LinkedList<T>();
+                current = list.head;
+                while (current != null)
+                {
+                    copy.Add(current.Data);
+                    current = current.Next;
+                }
+
+
+                Node<T> nextAfterX = nodeX.Next;
+                current = copy.head;
+
+                while (current != null)
+                {
+                    nodeX.Next = new Node<T>(current.Data);
+                    nodeX = nodeX.Next;
+                    current = current.Next;
+
+                }
+                nodeX.Next = nextAfterX;
+
+                while (nextAfterX != null)
+                {
+                    list.tail = nextAfterX;
+                    nextAfterX = nextAfterX.Next;
+
+                }
+
+            }
+        }
+
+
+        // 7. Функция, которая удаляет из списка все элементы E, если таковые имеются
+        public static void RemoveAll<T>(this LinkedList<T> list, T e)
+        {
+            while (list.Remove(e)) { }
+        }
+
+
+        // 9. Функция, которая дописывает к списку L список E
+        public static void AppendList<T>(this LinkedList<T> listL, LinkedList<T> listE)
+        {
+            if (listE.head != null)
+            {
+                if (listL.head == null)
+                {
+                    listL.head = listE.head;
+                    listL.tail = listE.tail;
+                }
+
+                else
+                {
+                    listL.tail.Next = listE.head;
+                    listL.tail = listE.tail;
+                }
+
+
+                listL.count += listE.Count;
+            }
+
+        }
+
+
+        // 11. Функция, которая удваивает список, приписывая его к самому себе
+        public static void Double<T>(this LinkedList<T> list)
+        {
+            LinkedList<T> copy = new LinkedList<T>();
+
+            Node<T> current = list.head;
+            while (current != null)
+            {
+                copy.Add(current.Data);
+                current = current.Next;
+            }
+
+            list.AppendList(copy);
+
+
+        }
+    }
+}
--- Lab3 dynamic structures/Program.cs
+++ Lab3 dynamic structures/Program.cs
@@ -1,25 +1,193 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Lab3_dynamic_structures
 {
     class Program
     {
-        public static void Main()
+        static LinkedList<int> currentList = new LinkedList<int>();
+
+        public static void Main(string[] args)
         {
-            string expression = "";
-            var list = RpnCalculator.Parse(expression);
+            bool running = true;
 
-            list = RpnCalculator.ToRPN(list);
-
-            foreach (var item in list)
+            while (running)
             {
-                Console.Write(item.ToString() + " ");
+                Console.WriteLine("\nВыберите действие:");
+                Console.WriteLine("1. Создать новый список");
+                Console.WriteLine("2. Загрузить список из файла");
+                Console.WriteLine("3. Перевернуть список");
+                Console.WriteLine("4. Посчитать уникальные элементы");
+                Console.WriteLine("5. Вставить список в себя");
+                Console.WriteLine("6. Удалить все элементы E");
+                Console.WriteLine("7. Добавить список к текущему");
+                Console.WriteLine("8. Удвоить список");
+                Console.WriteLine("9. Вывести список");
+                Console.WriteLine("0. Выход");
+
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        CreateNewList();
+                        break;
+                    case "2":
+                        LoadListFromFile();
+                        break;
+                    case "3":
+                        currentList.Reverse();
+                        Console.WriteLine("Список перевернут.");
+                        break;
+                    case "4":
+                        Console.WriteLine($"Уникальных элементов: {currentList.CountDistinct()}");
+                        break;
+                    case "5":
+                        InsertListIntoItself();
+                        break;
+                    case "6":
+                        RemoveAllElements();
+                        break;
+                    case "7":
+                        AppendList();
+                        break;
+                    case "8":
+                        currentList.Double();
+                        Console.WriteLine("Список удвоен.");
+                        break;
+                    case "9":
+                        Console.WriteLine("Список:");
+                        currentList.Print();
+                        break;
+                    case "0":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Некорректный ввод.");
+                        break;
+                }
             }
-
-            Console.WriteLine(RpnCalculator.Calculate(list));
-
-
         }
+
+
+        static void CreateNewList()
+        {
+            currentList = new LinkedList<int>();
+            Console.WriteLine("Новый список создан.");
+            AddElementsToList();
+        }
+
+        static void LoadListFromFile()
+        {
+            Console.Write("Введите путь к файлу: ");
+            string filePath = Console.ReadLine();
+
+            try
+            {
+                currentList = new LinkedList<int>();
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    if (int.TryParse(line, out int num))
+                    {
+                        currentList.Add(num);
+                    }
+                }
+                Console.WriteLine("Список загружен из файла.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при загрузке файла: {ex.Message}");
+            }
+        }
+
+
+
+
+        static void AddElementsToList()
+        {
+            Console.WriteLine("Вводите числа (для завершения введите пустую строку):");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) break;
+
+                if (int.TryParse(input, out int num))
+                {
+                    currentList.Add(num);
+
+                }
+
+
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Введите число.");
+                }
+            }
+        }
+
+        static void InsertListIntoItself()
+        {
+            Console.Write("Введите число x после которого список должен быть вставлен: ");
+            if (int.TryParse(Console.ReadLine(), out int x))
+            {
+                currentList.InsertIntoItself(x);
+                Console.WriteLine("Список вставлен.");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+        }
+
+
+        static void RemoveAllElements()
+        {
+            Console.Write("Введите элемент E для удаления: ");
+            if (int.TryParse(Console.ReadLine(), out int e))
+            {
+                currentList.RemoveAll(e);
+                Console.WriteLine("Элементы удалены.");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+        }
+
+        static void AppendList()
+        {
+            Console.WriteLine("Создайте новый список для добавления:");
+            LinkedList<int> newList = new LinkedList<int>();
+            AddElementsToListNew(newList);
+
+
+            currentList.AppendList(newList);
+            Console.WriteLine("Список добавлен.");
+        }
+
+        static void AddElementsToListNew(LinkedList<int> list)
+        {
+            Console.WriteLine("Вводите числа (для завершения введите пустую строку):");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) break;
+
+                if (int.TryParse(input, out int num))
+                {
+                    list.Add(num);
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Введите число.");
+                }
+            }
+        }
+
     }
 }
